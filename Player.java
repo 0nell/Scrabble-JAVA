6aa55bb833ae35995e8dceef8e9c8f86681b7977
AhmedJouda2000 <ahmed_judah@yahoo.com>
2020-02-07 10:04:36 +0000

    test

---
+++ Player.java
@@ -0,0 +1,69 @@
+
+
+public class Player {
+
+	private String name;
+	private int score;
+	Frame frame;
+	
+	
+	public Player(String name, int score, Frame frame) {
+		super();
+		this.name = name;
+		this.score = score;
+		this.frame = frame;
+	}
+
+	
+	//Allows display of player's name
+	public String getName() {
+		if (name != null) {
+			return name;
+		}
+		else return "";
+	}
+
+
+	//Allows player name to be set
+	//If the first character is a space or new line reject it
+	public void setName(String name) {
+		if (Character.isWhitespace(name.charAt(0))) {
+			throw new IllegalArgumentException();
+		}
+		else this.name = name;
+	}
+
+
+	//Allows access to the score
+	public int getScore() {
+		return score;
+	}
+
+
+	//Allows score to be increased
+	public void increaseScore(int plusScore) {
+		this.score = this.score + plusScore;
+	}
+	
+	//Allows data reset
+	public void reset() {
+		score = 0;
+		name = null;
+	}
+	
+	
+	//displays the player's frame
+	public void getFrame() {
+		frame.displayFrame();
+	}
+
+
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return "The player's name is: " + getName();
+	}
+
+		
+	}
