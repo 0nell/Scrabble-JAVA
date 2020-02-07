54506754a08f8e344cf253b077062118f0110890
seanm75 <sean.mcdonnell1@ucdconnect.ie>
2020-02-07 10:29:41 +0000

    static

--- Pool.java
+++ Pool.java
@@ -4,13 +4,13 @@ public class Pool
 
 	private static final String alph = "ABCDEFGHIJKLMNOPQRSTUVWXYZ ";
 	private static final int [] score = {1,3,3,2,1,4,2,4,1,8,5,1,3,1,1,3,10,1,1,1,1,4,4,8,4,10,0};
-	private int [] amount = {9,2,2,4,12,2,3,2,9,1,1,4,2,6,8,2,1,6,4,6,4,2,2,1,2,1,2};
+	private static int [] amount = {9,2,2,4,12,2,3,2,9,1,1,4,2,6,8,2,1,6,4,6,4,2,2,1,2,1,2};
 	private static final int [] res = {9,2,2,4,12,2,3,2,9,1,1,4,2,6,8,2,1,6,4,6,4,2,2,1,2,1,2};
-	private int total = 100;
+	private static int total = 100;
 	
 	
 	
-	public int getValue(char c)
+	public static int getValue(char c)
 	{
 		if(c == ' ')
 		{
@@ -22,7 +22,7 @@ public class Pool
 	}
 	
 	
-	public void reset() 
+	public static void reset() 
 	{
 		for(int i = 0;i < 27;i++)
 		{
@@ -32,13 +32,13 @@ public class Pool
 	}
 	
 	
-	public int remaining()
+	public static int remaining()
 	{
 		return total;
 	}
 	
 	
-	public char draw()
+	public static char draw()
 	{
 		char a = ' ';
 		boolean done = false;
@@ -58,7 +58,7 @@ public class Pool
 		return a;
 	}
 	
-	public char draw(char a)
+	public static char draw(char a)
 	{
 		if(a == ' ')
 		{
