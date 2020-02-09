e656e3d6440724527b23e171548b820402c542d3
seanm75 <sean.mcdonnell1@ucdconnect.ie>
2020-02-09 00:42:28 +0000

    minor changes

--- PlayerTest.java
+++ PlayerTest.java
@@ -9,8 +9,8 @@ public class PlayerTest {
 
 	public static void main(String[] args) {
 		PlayerTest test = new PlayerTest();
-		//test.poolClassTest();
-		//test.frameClassTest();
+		test.poolClassTest();
+		test.frameClassTest();
 		test.playerClassTest();
 	}
 
@@ -161,7 +161,7 @@ public class PlayerTest {
 
 		// test getValue()
 		System.out.println(
-				"------------------------------------\nTest the getValue method\n------------------------------------");
+				"------------------------\nTest the getValue method\n------------------------");
 		System.out.println("\nExpected: 1");
 		System.out.println("Got: " + Pool.getValue('A'));
 		System.out.println("\nExpected: 10");
@@ -172,7 +172,7 @@ public class PlayerTest {
 		System.out.println("Got: " + Pool.getValue('B'));
 	
 
-		System.out.println("----------------------------------------------\nTest the reset, draw and getNumOfTiles methods\n----------------------------------------------");
+		System.out.println("--------------------------------------------------------\nTest the reset, draw, swapTile and getNumOfTiles methods\n--------------------------------------------------------");
 		System.out.println("\nReset the pool");
 		Pool.reset(); // resets the pool back to its full 100 tiles
 
@@ -187,7 +187,7 @@ public class PlayerTest {
 		System.out.println("\nExpected: 96");
 		System.out.println("Got: " + Pool.getNumOfTiles());
 	
-		System.out.println("\nswapped char in and drew " + Pool.draw('A'));
+		System.out.println("\nswapped char in and drew " + Pool.swapTile('A'));
 
 		System.out.println("\nExpected: 96");
 		System.out.println("Got: " + Pool.getNumOfTiles());
@@ -204,9 +204,19 @@ public class PlayerTest {
 		System.out.println("\nExpected: 100");
 		System.out.println("Got: " + Pool.getNumOfTiles());
 
-		System.out.println("------------------------------------------\nTest isEmpty method\n------------------------------------------");
+		System.out.println("-------------------\nTest isEmpty method\n-------------------");
 		System.out.println("\nExpected: false");
 		System.out.println("Got: " + Pool.isEmpty());
+
+		System.out.println("\nEmptied the loop using draw");
+
+		for(int i = 0; i< 100; i++)
+		{
+			Pool.draw();
+		}
+		System.out.println("\nExpected: true");
+		System.out.println("Got: " + Pool.isEmpty());
+
 
 
 	}
