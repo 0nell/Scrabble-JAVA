0ce0c7809e9e16ddd80830a0dda373a693e9224a
Lleno <launiv3rse@gmail.com>
2020-03-23 21:18:53 +0000

    Ui now displays frame and takes in input from text field

--- UI.java
+++ UI.java
@@ -1,55 +1,80 @@
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.*;
-import javafx.scene.layout.*;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.TilePane;
 
 
 
 public class UI {
     Scene game;
     TextField textBox;
+    Player currentPlayer;
+    TilePane framePane;
+    Frame frame;
+    Label label;
+    String[] text;
+    Board board;
+    Pool tempPool;
+
+    UI(){
+        board = new Board();
+        textBox = new TextField();
+        textBox.setOnAction(e->parseInput(textBox.getText()));
+        label = new Label("Enter text");
+        framePane  = new TilePane();
+        currentPlayer = new Player();
+        tempPool = new Pool();
+
+        for(int j = 0; j < 7; j++) {
+            framePane.getChildren().add(new Tile('_'));
+            framePane.setTileAlignment(Pos.TOP_LEFT);
+        }
+
+
+    }
+
+    public Player getCurrentPlayer() {
+        return currentPlayer;
+    }
+
+    public void setCurrentPlayer(Player currentPlayer) {
+        this.currentPlayer = currentPlayer;
+    }
+
+    public TilePane getFramePane() {
+        return framePane;
+    }
 
     public Frame getFrame() {
+
         return frame;
     }
 
     public void setFrame(Frame frame) {
         this.frame = frame;
+        framePane.getChildren().removeAll(framePane.getChildren());
+        for(int j = 0; j < frame.size(); j++) {
+            framePane.getChildren().add(getFrame().getTiles().get(j));
+        }
     }
-
-    Frame frame;
 
     public Label getLabel() {
         return label;
     }
 
-    Label label;
-
-    public String getText() {
+    public String[] getText() {
         return text;
     }
 
-    public void setText(String text) {
+    public void setText(String[] text) {
         this.text = text;
     }
-
-    String text;
 
     public Board getBoard() {
         return board;
     }
-
-    Board board;
-
-
-    UI(){
-        board = new Board();
-        textBox = new TextField();
-        textBox.setOnAction(e->setText(textBox.getText()));
-        label = new Label("Enter command");
-
-    }
-
 
     TilePane printBoard(){
         TilePane tilePane = new TilePane();
@@ -67,22 +92,50 @@ public class UI {
     GridPane printGame(){
         GridPane gridPane = new GridPane();
         gridPane.setHgap(10);
+        gridPane.setVgap(15);
         gridPane.add(printBoard(), 0,0);
         gridPane.add(textBox, 1,4);
         gridPane.add(label, 1,0);
-        gridPane.add(printFrame(), 0,4);
+        gridPane.add(getFramePane(), 0,4);
         return gridPane;
     }
     TilePane printFrame(){
-        TilePane tilePane = new TilePane();
-        for(int j = 0; j < 7; j++) {
-            tilePane.getChildren().add(getFrame().getTiles().get(j));
-            tilePane.setTileAlignment(Pos.TOP_LEFT);
-        }
-        return tilePane;
+        return framePane;
     }
 
-    void parseInput(String input){
-    	
+    void parseInput(String command){
+        text = command.split(" ", 0);
+        int i = 0;
+        if (!text[i].equals("QUIT") && !text[i].equals("PASS") && !text[i].equals("HELP") && !text[i].equals("EXCHANGE")) {
+            i++;
+            int x = Integer.parseInt(text[i++]);
+            int y = Integer.parseInt(text[i++]);
+            String direction = text[i++];
+            String word = text[i++];
+
+
+            board.placeWord(currentPlayer, word, x, y, direction);
+            board.printBoard();
+
+        }
+
+        if (text[i].equals("QUIT")) {
+            System.exit(0);
+        }
+
+        if (text[i].equals("HELP")) {
+            label.setText("Display help message here");
+
+        }
+
+        if (text[i].equals("PASS")) {
+            return;
+        }
+
+        if(text[i].equals("EXCHANGE")) {
+            String letters =  text[i++];
+            currentPlayer.getFrame().remove(letters);
+            currentPlayer.getFrame().refill(tempPool);
+        }
     }
 }
